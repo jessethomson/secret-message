fb029279d5dfe18e22ded8e170fac43292ccf39d
Jesse Thomson <jessethomson11@gmail.com>
2017-02-25 01:04:54 -0700

    removed request module and config routes

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var config = require('./routes/config');
 
 var app = express();
 
